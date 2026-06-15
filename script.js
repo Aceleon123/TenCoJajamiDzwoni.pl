e04509d275181a944a2f4def18f3ec4697da18f6
Ace <115834490+Aceleon123@users.noreply.github.com>
2026-06-15 23:17:43 +0200

    1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const serverIP = "TimCraft.aternos.me:37260";
+const serverIP = "oazasmp.projecthelix.ovh";
 const apiURL = `https://api.mcsrvstat.us/2/${serverIP}`;
 
 function formatMotd(rawMotd) {
